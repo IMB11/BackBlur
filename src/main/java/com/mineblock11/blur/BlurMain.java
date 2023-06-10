92bd210c7aa13f7489b14187053a8f3b9ad963be
Calum <contact@mineblock11.dev>
2023-06-10 19:49:51 +0100

    1.20

--- src/main/java/com/mineblock11/blur/BlurMain.java
+++ src/main/java/com/mineblock11/blur/BlurMain.java
@@ -4,11 +4,7 @@ import io.wispforest.owo.client.OwoClient;
 import io.wispforest.owo.ui.core.Color;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
-import net.fabricmc.fabric.impl.client.screen.ScreenEventFactory;
-import net.fabricmc.fabric.impl.client.screen.ScreenExtensions;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
@@ -20,16 +16,16 @@ public class BlurMain implements ClientModInitializer {
     public void onInitializeClient() {
         CONFIG = com.mineblock11.blur.BlurConfig.createAndLoad();
         ScreenEvents.BEFORE_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-            ScreenEvents.beforeRender(screen).register((screen1, matrices, mouseX, mouseY, tickDelta) -> {
+            ScreenEvents.beforeRender(screen).register((screen1, context, mouseX, mouseY, tickDelta) -> {
                 if(client.world != null) {
-                    initiateBlur(matrices, 0, 0, screen1.width, screen1.height);
+                    initiateBlur(context, 0, 0, screen1.width, screen1.height);
                 }
             });
         });
 
     }
 
-    public static void initiateBlur(MatrixStack matrices, int x, int y, int width, int height) {
+    public static void initiateBlur(DrawContext context, int x, int y, int width, int height) {
         // TODO: tint color, different strength and quality.
 
         int quality = CONFIG.quality();
@@ -37,9 +33,10 @@ public class BlurMain implements ClientModInitializer {
         Color tintColor = CONFIG.tintColor();
         Color actualColor = new Color(tintColor.red(), tintColor.green(), tintColor.blue(), CONFIG.tintStrength());
 
-        DrawableHelper.fill(matrices, x, y, width, height, actualColor.argb());
+        context.fill(x, y, width, height, actualColor.argb());
 
         var buffer = Tessellator.getInstance().getBuffer();
+        var matrices = context.getMatrices();
         var matrix = matrices.peek().getPositionMatrix();
 
         buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
